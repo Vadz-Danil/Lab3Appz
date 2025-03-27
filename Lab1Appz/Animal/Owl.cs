a647a64110a585d511cf2795f07f04ea5e7c653b
Danil Vadzinskyi <vadz.danil@gmail.com>
2025-03-27 15:34:15 +0200

    Change Owl

--- Lab1Appz/Animal/Owl.cs
+++ Lab1Appz/Animal/Owl.cs
@@ -2,7 +2,7 @@ namespace Lab1Appz.Animal;
 
 public class Owl : Animal
 {
-    public Owl(string? name, bool isWild = false) : base(name, 2, 2, isWild)
+    public Owl(string? name, bool isWild = true) : base(name, 2, 2, isWild)
     {
     }
 }
